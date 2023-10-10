14f50a7ab501f09ff259008d9ba2fc1c1e1fd480
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-10-10 21:26:43 +0200

    Rust test

---
+++ rust/test/hello/src/main.rs
@@ -0,0 +1,13 @@
+fn efefe(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn main() {
+    let sum = 2 + 2;
+    let value = 10 - 5;
+    let division = 10 / 2;
+    let multi = 5 * 4;
+    let five = efefe(25, 20);
+    let modulo = 25 % 5;
+    let modulo2 = 25 % 6;
+}
