2d8de8c736a32e4e6dc4418663d48dcdd84d1019
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-10-14 17:08:50 +0200

    Exercises

--- rust/book_trpl/guess_game/src/main.rs
+++ rust/book_trpl/guess_game/src/main.rs
@@ -1,14 +1,33 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the fucking number Mason!");
     let secret_num = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is = {:?}", secret_num);
+    //println!("The secret number is = {:?}", secret_num);
     println!("If you guess wrong, you now what happen next");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the damn line");
-    println!("You guessed {guess}");
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the damn line");
+        println!("You guessed {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too smol"),
+            Ordering::Greater => println!("Too bigus"),
+            Ordering::Equal => {
+                println!("YOU WIN MASON FOR FUCK SAKES");
+                break;
+            }
+        }
+    }
 }
